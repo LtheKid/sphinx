65d2e92a97903359d7db748662f2ac728fd9d5f9
LtheKid <43533954+LtheKid@users.noreply.github.com>
2020-09-08 22:06:30 +0800

    update

--- sample_python_module.py
+++ sample_python_module.py
@@ -1,6 +1,7 @@
 def foo():
     '''
     This is a docstring. Note for this template the empty lines must be present!
+    If typing in notepad, then you need 4 spaces after the function and 7 spaces after code-block
     We then use highlight and code-block to encapsulate example code:
 
     .. highlight:: python
